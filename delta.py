abaa1020aa121cd07d6d229e07c0f5c19f5186d8
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-06-27 21:59:42 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -73,4 +73,5 @@ st.title('My title')
 st.header('My header')
 st.subheader('My sub')
 st.code('for i in range(8): foo()')
-* optional kwarg unsafe_allow_html = True
+
+optional kwarg unsafe_allow_html = True
