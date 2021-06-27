a07929f32d49b62981e1132770bc15ec862a7ba6
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-06-27 23:08:18 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -75,7 +75,5 @@ delta_A = delta(usd = invest , fix_value = fix_value , pair_data = pair_data , t
 delta_A= delta_A.cf()
 
 
-data = delta_A[['cf_change' , 'change_mkt' , '0' ]]
 
-plt.plot(data)
-st.pyplot()
+st.line_chart(delta_A[['cf_change' , 'change_mkt' , '0' ]])
