6e9a5ab7c8904911de12ee3e11ad7cfa9c2cb6bf
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-06-27 23:06:25 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt  
-import seaborn as sns
-sns.set()
 import pandas_datareader  as pdr
 import ccxt
 import streamlit as st
@@ -78,5 +76,5 @@ delta_A= delta_A.cf()
 
 data = delta_A[['cf_change' , 'change_mkt' , '0' ]]
 
-sns.lineplot(data=data)
-st.pyplot(data)
+plt.plot(data)
+st.pyplot()
