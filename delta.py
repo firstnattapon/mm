08c4578690a592d9701ec4de451e6ef25b290db4
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-14 22:27:53 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -27,7 +27,7 @@ class  delta :
         ohlcv = exchange.fetch_ohlcv(self.p_data, self.timeframe, limit=self.limit)
         ohlcv = exchange.convert_ohlcv_to_trading_view(ohlcv)
         df = pd.DataFrame(ohlcv)
-        df.t = df.t.apply(lambda x: datetime.fromtimestamp(x))
+        df.t = df.t.apply(lambda x: datetime.datetime.fromtimestamp(x))
         df = df.set_index(df['t'])
         df.t = df.index.dayofyear
         df = df.loc[df.t >= self.start_end[0]]
