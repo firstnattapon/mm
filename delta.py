bd4183d138390ed3c1114f9106b3cb95e969cfc6
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-15 21:54:59 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -142,18 +142,13 @@ options  = st.radio('options',
                      'cash_hold vs cash_mkt' ,
                      'sumusd_hold vs sumusd_mkt'] ,index=0 )
     
-if options == 'cashflow_hold':plot = ['cf_change' ,'price_change']
+if options == 'cashflow_hold':plot = ['cf_change']
 elif options == 'rebalancing':plot = ['re']
-elif options == 'port-value_change vs price_change':
-    plot = ['pv_change' ,'price_change']
-elif options == 'amount_hold vs amount_mkt':
-    plot = ['amount' ,'amount_mkt']
-elif options == 'asset-value_hold vs asset-value_mkt':
-    plot = ['asset_value' ,'assetvalue_mkt']
-elif options == 'cash_hold vs cash_mkt':
-    plot = ['cash' ,'cash_mkt']
-elif options == 'sumusd_hold vs sumusd_mkt':
-    plot = ['sumusd' ,'sumusd_mkt']
+elif options == 'port-value_change vs price_change':plot = ['pv_change' ,'price_change']
+elif options == 'amount_hold vs amount_mkt':plot = ['amount' ,'amount_mkt']
+elif options == 'asset-value_hold vs asset-value_mkt':plot = ['asset_value' ,'assetvalue_mkt']
+elif options == 'cash_hold vs cash_mkt':plot = ['cash' ,'cash_mkt']
+elif options == 'sumusd_hold vs sumusd_mkt':plot = ['sumusd' ,'sumusd_mkt']
     
 _ = delta_A[plot] #  ; _.columns = ['1: cf_%', '2: mkt_%' , "3: zero_line"] 
 
