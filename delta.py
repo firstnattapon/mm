006efd9f561f76d69a72138ad6c025bf2e441550
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-15 22:20:52 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -9,6 +9,8 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 from datetime import datetime
 st.set_option('deprecation.showPyplotGlobalUse', False)
 import datetime
+import seaborn as sns
+sns.set_theme(style="darkgrid")
 
 class  delta :
     def __init__(self , usd = 1000 , fix_value = 0.50, p_data = 'ALPHA-PERP', timeframe = '15m' 
@@ -149,10 +151,13 @@ elif options == 'amount_hold vs amount_mkt':plot = ['amount' ,'amount_mkt']
 elif options == 'asset-value_hold vs asset-value_mkt':plot = ['asset_value' ,'assetvalue_mkt']
 elif options == 'cash_hold vs cash_mkt':plot = ['cash' ,'cash_mkt']
 elif options == 'sumusd_hold vs sumusd_mkt':plot = ['sumusd' ,'sumusd_mkt']
-    
-_ = delta_A[plot]  #; _.columns = ['1: cf_%', '2: mkt_%' , "3: zero_line"] 
-# _ = delta_A[['cf_change' ,'price_change' ,'0' ]] #  ; _.columns = ['1: cf_%', '2: mkt_%' , "3: zero_line"] 
-st.line_chart(_)
+  
+sns.lineplot(x= plot , y=delta_A.index , data=delta_A)
+st.pyplot()
+
+# _ = delta_A[plot]  #; _.columns = ['1: cf_%', '2: mkt_%' , "3: zero_line"] 
+# # _ = delta_A[['cf_change' ,'price_change' ,'0' ]] #  ; _.columns = ['1: cf_%', '2: mkt_%' , "3: zero_line"] 
+# st.line_chart(_)
 
 st.write('x0 :' , 0.45  , '   ,   r :' , mu , '   ,   n :' , n  ,'   ,   max :' , max)
 st.write('data :' , len(delta_A) , '   ,   start :' , start , '   ,   end :' , end ,
