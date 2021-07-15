12bba98ec038ea3215ffb6fa6bf959bf6a1dbcfc
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-15 22:36:31 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -7,9 +7,9 @@ import streamlit as st
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 from datetime import datetime
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# st.set_option('deprecation.showPyplotGlobalUse', False)
 import datetime
-plt.style.use('ggplot')
+# plt.style.use('ggplot')
 
 class  delta :
     def __init__(self , usd = 1000 , fix_value = 0.50, p_data = 'ALPHA-PERP', timeframe = '15m' 
