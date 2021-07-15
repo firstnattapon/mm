6e4116f70ab42c200e9694c9e17c9630c5b4bd2f
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-15 22:31:14 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -151,7 +151,7 @@ elif options == 'asset-value_hold vs asset-value_mkt':plot = ['asset_value' ,'as
 elif options == 'cash_hold vs cash_mkt':plot = ['cash' ,'cash_mkt']
 elif options == 'sumusd_hold vs sumusd_mkt':plot = ['sumusd' ,'sumusd_mkt']
   
-plt.subplots(figsize=(15, 10))
+plt.subplots(figsize=(12, 8))
 plt.plot(delta_A[plot])
 st.pyplot()
 
