d4fd0ce499f8023af69dff50c9e7cc19fe8e8862
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-15 22:47:25 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -145,7 +145,7 @@ options  = st.radio('options',
     
 if options == 'cashflow_hold':plot = ['cf_change' , '0']
 elif options == 'rebalancing':plot = ['re' , "0"]
-elif options == 'port-value_change vs price_change':plot = ['pv_change' ,'price_change']
+elif options == 'port-value_change vs price_change':plot = ['pv_change' ,'price_change' , "0"]
 elif options == 'amount_hold vs amount_mkt':plot = ['amount' ,'amount_mkt']
 elif options == 'asset-value_hold vs asset-value_mkt':plot = ['asset_value' ,'assetvalue_mkt']
 elif options == 'cash_hold vs cash_mkt':plot = ['cash' ,'cash_mkt']
@@ -156,10 +156,6 @@ for i in plot:
     plt.plot(delta_A[i] , label =i)
 plt.legend()
 st.pyplot()
-
-# _ = delta_A[plot]  #; _.columns = ['1: cf_%', '2: mkt_%' , "3: zero_line"] 
-# # _ = delta_A[['cf_change' ,'price_change' ,'0' ]] #  ; _.columns = ['1: cf_%', '2: mkt_%' , "3: zero_line"] 
-# st.line_chart(_)
 
 st.write('x0 :' , 0.45  , '   ,   r :' , mu , '   ,   n :' , n  ,'   ,   max :' , max)
 st.write('data :' , len(delta_A) , '   ,   start :' , start , '   ,   end :' , end ,
