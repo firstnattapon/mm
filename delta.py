80a16f45a9fa2275186d66e571776da94f925721
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-15 19:36:04 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -138,8 +138,8 @@ _ = delta_A[[ 'pv_change', 'price_change' , '0' ]] ; _.columns = ['1: pv_%', '2:
 st.line_chart(_)
 
 st.write('data :' , len(delta_A))
-st.write('cf_change :'  , round(delta_A['cf_change'][-1] , 2),'%' , ' ,  cf_usd :',  round(float(delta_A['cf_usd'][-1]) , 2 ) ,'$')
-st.write('amount :'  , round(delta_A['amount'][-1] , 2) , ' ,  amount :',  round(float(delta_A['cf_usd'][-1]) , 2 ) )
+st.write('cf_change :'  , round(delta_A['cf_change'][-1] , 2),'%' ,' ,  cf_usd :',  round(float(delta_A['cf_usd'][-1]) , 2 ) ,'$')
+st.write('amount :'  , round(delta_A['amount'][-1] , 2) , ' ,  amount_mkt :',  round(delta_A['amount_mkt'][-1] , 2)  )
 st.write('')
 
 _, _ , head , _ ,   = st.beta_columns(4) 
