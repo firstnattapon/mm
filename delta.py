718b89f87585b03532e4153de0a0c9de7e01ac93
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-17 23:39:18 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -154,8 +154,8 @@ end = col9.date_input('end', datetime.date(2021,7,31)) ; end =  int(end.timetupl
 
 col10 , col11 = st.beta_columns(2)
 with col10.beta_expander("Feigenbaum "):
-    λ = st.slider('λ', min_value=0.0 , max_value=4.0 , value=1.865 , format="%.3f" )
-    X0 = st.slider('X0', min_value=0.0 , max_value=1.0 , value=0.50 , format="%.2f" )    
+    λ = st.slider('λ', min_value=0.0 , max_value=4.0 , value=3.90  , format="%.3f" )
+    X0 = st.slider('X0', min_value=0.0 , max_value=1.0 , value=0.5  , format="%.2f" )    
     N = st.slider('N', min_value=0 , max_value=20000 , value=9999) 
 
 y = [] ; x = X0 ; mu = λ ; num = int(N)
@@ -198,9 +198,13 @@ if 1 :
              round(delta_A['re'][-1] , 2) , '   ,   diff :' , round(delta_A['diff'][-1] , 4 )
             )
 
+    
     plt.subplots(figsize=(12, 8))
     for i in plot:
         plt.plot(delta_A[i] , label =i)
+     
+#     delta_A = delta_A[delta_A['index'] in ]
+        
     plt.legend()
     st.pyplot()
 
