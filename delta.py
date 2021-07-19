9e829cdb6bdf1d70d1112fea96d45fd64116780b
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-19 21:47:08 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -203,7 +203,7 @@ if 1 :
 
     st.write('index :' , delta_A['index'][-1] , 
 #              '   ,   next_re :' ,  [i if i > delta_A['index'][-1] else np.nan for i in delta_x.series_num][0]  ,
-             '   ,   next_re :' ,  [  i for i in delta_x.series_num if i > delta_A['index'][-1] else np.nan ][0]  ,
+             '   ,   next_re :' ,  [  i for i in delta_x.series_num if i > delta_A['index'][-1] else 0 [0]  ,
 
              '   ,   start :' , start , '   ,   end :' , end ,
              '   ,   perdit :',delta_A['perdit'][-1] ,'   ,   re :' ,
