3196d123e6ad6364aebce5b25d11288e77570076
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-20 21:26:12 +0700

    Update delta.py

--- delta.py
+++ delta.py
@@ -168,18 +168,16 @@ with col10.beta_expander("Feigenbaum "):
         delta_z = delta(p_data = pair_data , start_end=[start  , end] , max= max , linear=True)
         cf0 =  0   ; 
         
-        for _ in stqdm(range(1), st_container=st.sidebar):
-            for  Index , _  in enumerate(delta_z.series_num):
-                if len(delta_z.get_data()) >= Index :
-                    delta_z.series_num.pop(Index)
-                    delta_df = delta_z.final()
-                    cf1 = delta_df['cf_usd'][-1]
-                    if cf0 < cf1 :
-                        cf0 =  cf1
-                        print( cf1 )
-                    else:
-                        delta_z.series_num.append(Index)
-                else: break
+        for  Index , _  in enumerate(delta_z.series_num):
+            if len(delta_z.get_data()) > Index :
+                delta_z.series_num.pop(Index)
+                delta_df = delta_z.final()
+                cf1 = delta_df['cf_usd'][-1]
+                if cf0 < cf1 :
+                    cf0 =  cf1
+                else:
+                    delta_z.series_num.append(Index)
+            else: break
             
         y = delta_z.series_num
         
