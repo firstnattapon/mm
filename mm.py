e9419aaa78c491cb1eef5da28ff675d4ae348b41
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-28 22:39:22 +0700

    Update mm.py

--- mm.py
+++ mm.py
@@ -77,7 +77,7 @@ col4.write('upper = {:.2f}'.format( p[index_3]))
 col4.write('cf = {:.2f}'.format( pf[3])) 
 
 col6,  = st.beta_columns(1)
-df = pd.DataFrame({ "ราคา" : p })
+df = pd.DataFrame({ "ราคา" : p  , "asset": a })
 
 with  col6.beta_expander('data'):
     st.write(df)
