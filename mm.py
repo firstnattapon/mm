0840e0fd2daf44619fda4d71c84d5377f3e71800
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-28 23:12:37 +0700

    Update mm.py

--- mm.py
+++ mm.py
@@ -17,6 +17,8 @@ np.set_printoptions(precision=8)
 col1,  = st.beta_columns(1)
 col5,  = st.beta_columns(1)
 col2,col3,col4  = st.beta_columns(3)
+col7,  = st.beta_columns(1)
+
 
 with  col3.beta_expander(''):
     capital = st.number_input('capital', min_value=0, max_value=100000 , value=1000 )
